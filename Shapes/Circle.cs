00ab9c5ca607a6cea992f90eef8b7e97c59fab4a
Vladimir Danilin <volodyadanilin@gmail.com>
2024-05-22 11:26:25 +0300

    Code Comments Added

--- Shapes/Circle.cs
+++ Shapes/Circle.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ShapeAreaCalculation
+﻿
+namespace ShapeAreaCalculation.Shapes
 {
     public class Circle : Shape
     {
